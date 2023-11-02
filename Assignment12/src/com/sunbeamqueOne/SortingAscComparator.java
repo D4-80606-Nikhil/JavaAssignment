373871dcd4b380324fb0d31dd396638da024f56a
Nikhilchauhan3011 <nikhilchauhan3011@gmail.com>
2023-11-02 21:58:32 +0530

    Work in Progress

---
+++ Assignment12/src/com/sunbeamqueOne/SortingAscComparator.java
@@ -0,0 +1,13 @@
+package com.sunbeamqueOne;
+
+import java.util.Comparator;
+
+public class SortingAscComparator implements Comparator<Book>{
+
+	@Override
+	public int compare(Book arg0, Book arg1) {
+		// TODO Auto-generated method stub
+		return arg0.getIsbn().compareTo(arg1.getIsbn());
+	}
+
+}
---
+++ Assignment12/src/com/sunbeamqueOne/SortingDescComparator.java
@@ -0,0 +1,13 @@
+package com.sunbeamqueOne;
+
+import java.util.Comparator;
+
+public class SortingDescComparator implements Comparator<Book>{
+
+	@Override
+	public int compare(Book arg0, Book arg1) {
+		// TODO Auto-generated method stub
+		return -arg0.getIsbn().compareTo(arg1.getIsbn());
+	}
+
+}
