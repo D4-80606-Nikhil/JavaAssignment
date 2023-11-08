531ff2711c674ad352f6e75fa59a572dd11acf72
Nikhilchauhan3011 <nikhilchauhan3011@gmail.com>
2023-11-08 18:12:20 +0530

    Work in Progress

---
+++ Assignment09/src/com/sunbeam/quesix/Person.java
@@ -0,0 +1,22 @@
+package com.sunbeam.quesix;
+
+
+enum Gender{
+	MALE,FEMALE
+}
+
+public class Person {
+   private String name;
+   private int age;
+   private Gender gender;
+   
+   
+   
+   
+   public static void main(String ...args) {
+	   Person[] person = new Person[10];
+	   
+	   
+   }
+   
+}
